b0d2086d53583941e9e8d065fd9c96825187e687
kanyasrithong <ks4264@drexel.edu>
2025-11-06 14:35:30 -0500

    Add Header to layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,7 @@
 <script>
 	import favicon from '$lib/assets/favicon.svg';
+	import Header from "$lib/Header.svelte";
+	import '$lib/global.css';
 
 	let { children } = $props();
 </script>
@@ -8,4 +10,5 @@
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
+<Header></Header>
 {@render children?.()}

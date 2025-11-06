4c47cd90401e069eddcefd68b094ff8477ada48e
kanyasrithong <ks4264@drexel.edu>
2025-11-06 14:34:55 -0500

    Header component + styles

---
+++ src/lib/Header.svelte
@@ -0,0 +1,30 @@
+<script lang="js">
+  import ShoppingBag from "$lib/ShoppingBag.svelte";
+</script>
+
+<div id="header">
+  <a href="/">
+    kanya's things
+  </a>
+  <ShoppingBag></ShoppingBag>
+</div>
+
+<style>
+  div {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 1rem;
+
+    a {
+      color: black;
+      font-family: Satoshi;
+      font-size: 24px;
+      text-decoration: none;
+    }
+
+    a:visited {
+    text-decoration: none;
+  }
+  }
+</style>

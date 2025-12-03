20eaeedcbbf385fe79566259fedb5f08d908e088
kanyasrithong <ks4264@drexel.edu>
2025-12-03 15:34:14 -0500

    Item card component + styles

---
+++ src/lib/components/ItemCard.svelte
@@ -0,0 +1,48 @@
+<script lang="js">
+  import angelWings from "$lib/assets/images/angel-wings.png";
+</script>
+
+<div>
+  <h2>Angel Wings</h2>
+  <img src={angelWings} alt="">
+  <img src="" alt="">
+  <h1>$111</h1>
+</div>
+
+<style>
+  div {
+    display: flex;
+    flex-direction: column;
+
+    aspect-ratio: 0.8 / 1;
+    background-color: #111111;
+    color: white;
+    padding: 1rem;
+    position: relative;
+    width: 100%;
+  }
+
+  div:hover {
+    cursor: pointer;
+  }
+
+  h1 {
+    display: inline-flex;
+    flex-direction: row-reverse;
+    position: absolute;
+    bottom: 1rem;
+    right: 1rem;
+  }
+
+  img {
+    object-fit: scale-down;
+    position: absolute;
+    left: 0;
+    right: 0;
+    top: 0;
+    bottom: 0;
+    margin: 0 auto;
+    height: 100%;
+    width: 100%;
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,29 @@
+<script>
+	import ItemCard from "$lib/components/ItemCard.svelte";
 
+</script>
+
+<div>
+  <ItemCard/>
+  <ItemCard/>
+  <ItemCard/>
+  <ItemCard/>
+  <ItemCard/>
+  <ItemCard/>
+  <ItemCard/>
+  <ItemCard/>
+  <ItemCard/>
+  <ItemCard/>
+</div>
+
+<style>
+  div {
+    box-sizing: border-box;
+    display: grid;
+    grid: repeat(4, auto) / repeat(3, minmax(0,1fr));
+    gap: 10px;
+    padding: var(--header-height) 0.5rem 0;
+    width: 100vw;
+  }
+</style>
 
